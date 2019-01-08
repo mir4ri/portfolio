c45a46894440e9d40fa856e332a6a39243a4248e
Leo Buchko <leobuchko@gmail.com>
2019-01-09 00:15:40 +0200

    minor design updates

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
-// Scroll
-       $('a[href*="#"]')
-           .not('[href="#"]')
-           .not('[href="#0"]')
-           .click(function (event) {
-               if (
-                   location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-                   location.hostname == this.hostname
-               ) {
-                   var target = $(this.hash);
-                   target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                   if (target.length) {
-                       event.preventDefault();
-                       var menuHeight = $("html").outerHeight(true);
-                       $('html, body').animate({
-                           scrollTop: target.offset().top
-                       }, 1000);
-                   }
-               }
-           });
+// #croll
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        event.preventDefault();
+        var menuHeight = $("html").outerHeight(true);
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+      }
+    }
+  });
